30dcfb5f6649538b2ed3c7dd390e9185e6ce9bb2
Muhsin pk <Muhsinwazza@gmail.com>
2019-11-11 21:38:49 +0530

    Crypto
    Added crypto library and a test api

--- api/api/controllers/User.js
+++ api/api/controllers/User.js
@@ -5,6 +5,8 @@ var MongoClient = mongo.MongoClient;
 var config = require('./Assets/Config');
 var url = config.dbUrl();
 var mailer = require('./Assets/MailHelper.js');
+const crypto = require('cryptr');
+
 exports.RegisterUser = function(req,res)
 {
     MongoClient.connect(url, function(err, db) {
@@ -31,4 +33,16 @@ exports.RegisterUser = function(req,res)
         });
         
       });
+}
+exports.crypt =function(req,res)
+{
+  const Cryptr = require('cryptr');
+  const cryptr = new Cryptr('myTotalySecretKey');
+   
+  const encryptedString = cryptr.encrypt('muhsinwazza@gmail.com');
+  const decryptedString = cryptr.decrypt(encryptedString);
+   
+  console.log(encryptedString); // e7b75a472b65bc4a42e7b3f78833a4d00040beba796062bf7c13d9533b149e5ec3784813dc20348fdf248d28a2982df85b83d1109623bce45f08238f6ea9bd9bb5f406427b2a40f969802635b8907a0a57944f2c12f334bd081d5143a357c173a611e1b64a
+  console.log(decryptedString);
+  res.json("success");
 }
