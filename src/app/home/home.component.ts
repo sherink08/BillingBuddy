ddd7b07c04d6867477e791448bb3ee319f8c6e1d
Muhsin pk <Muhsinwazza@gmail.com>
2019-11-15 16:49:44 +0530

    design change v2
    design change v2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,4 +12,16 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  type()
+  {
+    $('.chat-controls button').hide();
+    $('.chat-controls span:first-child').show();
+    $('.chat-controls').css( "grid-template-columns", "25px minmax(0, 1fr) 25px" );
+  }
+  hideType(){
+    $('.chat-controls button').show();
+    $('.chat-controls span:first-child').hide();
+    $('.chat-controls').css( "grid-template-columns", "70px 90px minmax(0, 1fr) 25px" );
+  }
+
 }
