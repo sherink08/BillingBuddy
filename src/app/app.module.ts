8d6b733aeeadb3bca35d19e2d1622e2f31fb7526
Sherin K <sherin.k@bluechip.com>
2019-11-22 12:33:20 +0530

    added Jquery dev dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './services/auth.guard';
-import {HttpInterceptorService} from './services/http-interceptor.service';
+import { HttpInterceptorService } from './services/http-interceptor.service';
+import * as $ from 'jquery';
 
 @NgModule({
   declarations: [
