7f57c149dc54693b44ff24eded180a513ef30b4a
Sherin K <sherin.k@bluechip.com>
2019-11-28 23:03:11 +0530

    Theme change, chat details enhancement

--- src/app/chat-details/chat-details.component.ts
+++ src/app/chat-details/chat-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-chat-details',
@@ -10,6 +10,7 @@ export class ChatDetailsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
--- src/app/chat-header/chat-header.component.ts
+++ src/app/chat-header/chat-header.component.ts
@@ -1,11 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input} from '@angular/core';
 
 @Component({
   selector: 'app-chat-header',
   templateUrl: './chat-header.component.html',
-  styleUrls: ['./chat-header.component.css']
+  styleUrls: ['./chat-header.component.css'],
 })
 export class ChatHeaderComponent implements OnInit {
+
+  @Input('currentBody') currentBody: string;
 
   constructor() { }
 
